cbdf45bdeb12ffb199ee725df0a43489574ce207
Adithya <73640313+adithya-vedhamani@users.noreply.github.com>
2023-01-10 15:07:16 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import Nav from "./components/Nav/Nav";
+import Footer from "./components/Nav/Footer";
+import appData from "./hooks/appData";
+import "./css/App.css";
+import "./css/Cryptos.css";
+import {Routes, Route} from "react-router-dom"
+import Cryptos from "./pages/Cryptos"
+import Portfolio from "./pages/Portfolio";
+import Widgets from "./pages/Widgets";
+import News from "./pages/News";
+
+
+function App() {
+  const { cryptos, globalData, trending, news } = appData();
+
+  return (
+    <div className="App">
+      <Nav globalData={globalData} />
+
+      <Routes>
+        <Route path="/" element={<Cryptos cryptos={cryptos} />} />
+        <Route path="/portfolio" element={<Portfolio cryptos={cryptos} />} />
+        <Route path="/news" element={<News news={news}/>} />
+        <Route path="/wallet" element={<News news={news}/>} />
+        <Route
+          path="/widgets"
+          element={
+            <Widgets
+              cryptos={cryptos}
+              globalData={globalData}
+              trending={trending}
+            />
+          }
+        />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Footer.tsx
@@ -0,0 +1,16 @@
+import "./Nav.css";
+import { Link } from "react-router-dom";
+
+export default function Footer(props: any) {
+    return (
+      <div className="Footer">
+        <div className="footer-main">
+          <p className="footer-copyright">
+            © 2023 CryptoAnalysis. All rights reserved
+          </p>
+        </div>
+
+       
+      </div>
+    );
+}
